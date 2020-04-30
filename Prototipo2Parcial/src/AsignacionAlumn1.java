690123b96c5474e525e869f86afc35357a9074d7
NydiLion <60206568+NydiLion@users.noreply.github.com>
2020-04-30 11:46:01 -0600

    txtfiel y botones

--- Prototipo2Parcial/src/AsignacionAlumn1.java
+++ Prototipo2Parcial/src/AsignacionAlumn1.java
@@ -40,11 +40,11 @@ public class AsignacionAlumn1 extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 675, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 455, Short.MAX_VALUE)
         );
 
         pack();
--- Prototipo2Parcial/src/AsignacionMaestros1.java
+++ Prototipo2Parcial/src/AsignacionMaestros1.java
@@ -41,11 +41,11 @@ public class AsignacionMaestros1 extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 717, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 460, Short.MAX_VALUE)
         );
 
         pack();
